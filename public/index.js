fccd54d7246e35dbd7f4b833ed122b69c71fb304
rejhr <hyoreong@reconers.com>
2025-04-16 11:14:27 +0900

    renamed:    src/index.js -> public/index.js

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./style.css";
-import App from "./App";
+import App from "../src/App";
 import { Leva } from "leva";
 
 createRoot(document.getElementById("root")).render(
